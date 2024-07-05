35eeef91a23a36c6d4dad7a4844314ec2a13facd
raghavankit100 <raghavankit100@gmail.com>
2024-07-05 22:26:34 +0530

    added simple component resources

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2>Topics Covered</h2>
+      <p id='para'>The following is a list of all the topics we cover in the MDN learning area.</p>
+      <br></br>
+      <a href=''>Getting started with the web</a>
+      <p>Provides a practical intruction to web developent to complete beginners.</p>
+      <br></br>
+      <a href=''>HTML -- Structurig the web</a>
+      <p>HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HRML in detail.</p>
+      <br></br>
+      <a href=''>Getting started with the web</a>
+      <p>Provides a practical intruction to web developent to complete beginners.</p>
+      <br></br>
+      <a href=''>CSS -- Styling the web</a>
+      <p>CSS is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.</p>
+    </>
+  )
+}
+
+export default App
